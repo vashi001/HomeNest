3db1270e31ce13e509245a5889984560dd3377b0
vashi001 <101816262+vashi001@users.noreply.github.com>
2024-08-13 16:15:36 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Deva Deva",
+    artist: "Arijit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Agar Tum Saath Ho",
+    artist: "Alka Yagnik & Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Naatu Naatu",
+    artist: "Rahul Sipligunj & Kaala Bhairava",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Vikram Title Track",
+    artist: "Anirudh Ravichander",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Kesariya",
+    artist: "Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Khairiyat",
+    artist: "Arijit Singh",
+    img: "music-6",
+    src: "music-6"
+  },
+];
