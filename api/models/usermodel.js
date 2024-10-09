7395d1a7a172b4e3e7afeb2dc4f39b42b7a5c281
vashi001 <vashishtapelapudi@gmail.com>
2024-10-09 22:20:51 +0530

    create usermodel

---
+++ api/models/usermodel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+// timestamp uses for sorting of items and when ever user is created or updated the time stamp gets changed
+
+const User = mongoose.model("User", userSchema);
+
+export default user;
