fea9b19c2135d2ca25d0e4ed32f7586093a2f7df
vashi001 <vashishtapelapudi@gmail.com>
2024-10-10 23:09:10 +0530

    create signin api routes

--- api/controller/authcontroller.js
+++ api/controller/authcontroller.js
@@ -1,6 +1,7 @@
 import User from "../models/usermodel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedpassword = bcryptjs.hashSync(password, 10);
@@ -13,5 +14,26 @@ export const signup = async (req, res, next) => {
     next(error);
     // next(errorHandler(550, "error from function"));
     // res.status(500).json(error.message);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errorHandler(404, "user Not Found"));
+    }
+    const ValidPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!ValidPassword) return next(errorHandler(401, " Wrong Credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    //._doc gives the correct exprected response other wise we get too many things from the response
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
