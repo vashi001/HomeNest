57bb632c1f84613d4c41a27b73fedbb8898817b5
vashi001 <vashishtapelapudi@gmail.com>
2024-10-10 15:45:55 +0530

    Complete signup page functionality

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userroute.js";
+import cors from "cors";
 // since we are using export default we can change the name here as there is only one export hence no error
 import authRouter from "./routes/authroute.js";
 dotenv.config();
@@ -15,12 +16,12 @@ mongoose
   });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 // the upper express.json() accepts the json data
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,30 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 export const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("http://localhost:3000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success == false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div className="p-3 max-w-lg">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-bold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4" onSubmit={(e) => handleSubmit(e)}>
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={(e) => handleChange(e)}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={(e) => handleChange(e)}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={(e) => handleChange(e)}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -33,6 +75,7 @@ export const Signup = () => {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 };
