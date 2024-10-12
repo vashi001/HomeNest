212425af4d76d452ccb1291416dd200883fdb0d4
vashi001 <vashishtapelapudi@gmail.com>
2024-10-12 16:49:29 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userroute.js";
 import cors from "cors";
 // since we are using export default we can change the name here as there is only one export hence no error
 import authRouter from "./routes/authroute.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -18,6 +19,7 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 // the upper express.json() accepts the json data
 app.listen(3000, () => {
   console.log("server is running on port 3000");
