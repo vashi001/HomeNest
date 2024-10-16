3eb8a7595981f9263ca5ab619f917588b81a83eb
vashi001 <vashishtapelapudi@gmail.com>
2024-10-16 16:02:19 +0530

    deployment changes

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import authRouter from "./routes/authroute.js";
 import listingroute from "./routes/listingroute.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -16,7 +17,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+const __dirname = path.resolve();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -28,6 +29,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingroute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
